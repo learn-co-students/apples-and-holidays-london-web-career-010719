f5d5b0137af7437452693763f1a3738c21860021
parik panchal <parikspanchal@icloud.com>
2019-01-01 21:05:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,17 +27,23 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  a = holiday_supplies[:winter][:christmas]
-  b = holiday_supplies[:winter][:new_years]
-  c = holiday_supplies[:summer][:fourth_of_july]
-  d = holiday_supplies[:fall][:thanksgiving]
-  e = holiday_supplies[:spring][:memorial_day]
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
+  #   Fourth Of July: Fireworks, BBQ
+  # etc.
 
-  f = a.concat(b)
-  g = f.concat(c)
-  h = g.concat(d)
-  i = h.concat(e)
-  i
+  holiday_supplies.each do |key, data|
+  puts "#{key}:"
+  data.each do |attribute, value|
+  print "   #{attribute}: "
+  value.each do |supply|
+  puts "#{supply}"
+  end
+  end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
