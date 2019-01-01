3cc77d615ec7c07834cfa4cbc6fe20a2530ec558
parik panchal <parikspanchal@icloud.com>
2019-01-01 21:20:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash
 end
 
-all_supplies_in_holidays(holiday_supplies)
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
