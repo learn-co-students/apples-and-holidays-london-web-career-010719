b2c10b0f8b8a044850c7a20316cbf40c48dfaeee
parik panchal <parikspanchal@icloud.com>
2019-01-01 20:13:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_supplies[:season][:holiday_name] = {}
   holiday_supplies[:seaspm][:holiday_name] = [].concat(supply_array)
+  holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
