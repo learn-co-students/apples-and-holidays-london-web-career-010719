0ace1d0d49c9f5c340546dcae45eb53c18c0c6d1
parik panchal <parikspanchal@icloud.com>
2019-01-01 21:41:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,13 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  warray = []
+  holiday_hash.each do |season, data|
+  data.each do |festivals, items|
+  #puts "#{festivals}: #{items}"
+  if items.include? "BBQ"
+  warray.push(festivals)
+  end
+  end
+  end
 end
