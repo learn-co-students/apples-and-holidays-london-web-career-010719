02328e3086ab27b19fda7c1a3dfc3ad7f5a44a2f
parik panchal <parikspanchal@icloud.com>
2019-01-01 21:22:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, festival|
-  puts "#{season.to_s.capitalize}:s"
+  puts "#{season.to_s.capitalize}:"
   festival.each do |festivall, supplies|
   print "    #{festivall.capitalize}: "
   print "#{supplies.join(", ")}"
