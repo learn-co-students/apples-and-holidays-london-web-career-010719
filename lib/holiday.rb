ec7089d792e506ec2ca03d56f219b6ce0e338db2
parik panchal <parikspanchal@icloud.com>
2019-01-01 21:15:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,16 +35,15 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |key, data|
-  puts "#{key}:"
-  data.each do |attribute, value|
-  print "   #{attribute}: "
-  value.each do |supply|
-  puts "#{supply}"
+  holiday_hash.each do |season, festival|
+  puts "#{season}: "
+  festival.each do |festivall, supplies|
+  print "#{festivall}: "
+  print "#{supplies.join(", ")}"
+  puts ""
   end
   end
-  end
-  holiday_hash
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
